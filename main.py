632be39fb9c30fa44d76307a47b96690197c22d7
okoestner <okoestner@web.de>
2020-09-05 18:28:21 +0200

    code restructured

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-import random
-
-geheimnis = random.randint(1,50)
-
-versuch = -1
-zeahler = 0
-
-while versuch != geheimnis:
-    versuch = int(input("du bist dran: "))
-    if versuch < geheimnis:
-        print("zu klein")
-
-    if versuch > geheimnis:
-        print( "zu groß")
-
-    zeahler = zeahler + 1
-
-print(" super, geschafft in ", zeahler, " versuchen.")
+import guessNumber
