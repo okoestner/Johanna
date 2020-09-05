7c54e7f1c279b23567d9a78970c7814885b2831b
okoestner <okoestner@web.de>
2020-09-05 15:19:41 +0200

    added the division script

---
+++ venv/division.py
@@ -0,0 +1,11 @@
+
+# Eingabe
+zahlEins = input("Erste Zahl: ")
+zahlZwei = input("Zweite Zahl: ")
+
+# Verarbeitung
+ergebnis = int(zahlEins) // int(zahlZwei)
+
+# Ausgabe
+print("Ergebnis der Division: ", ergebnis)
+
